a947691f1830598628aa866705e43359bef332f3
Naim Jeem <naim36208@gmail.com>
2017-07-30 23:40:38 +0600

    created some demo article objects

--- app.js
+++ app.js
@@ -10,9 +10,43 @@ app.set('view engine', 'pug');
 
 //Home Route
 app.get('/', (req, res) => {
+
+  let articles = [
+    {
+      id: 1,
+      title: 'First Article',
+      author: 'Naim Jeem',
+      body: `This is content for article one which is created by Naim Jeem`
+    },
+    {
+      id: 2,
+      title: 'Second Article',
+      author: 'Naim Jeem',
+      body: `This is content for article one which is created by Naim Jeem`
+    },
+    {
+      id: 3,
+      title: 'Third Article',
+      author: 'Naim Jeem',
+      body: `This is content for article one which is created by Naim Jeem`
+    },
+    {
+      id: 4,
+      title: 'Fourth Article',
+      author: 'Ibrahim Zahin',
+      body: `This is content for article one which is created by Naim Jeem`
+    },
+    {
+      id: 5,
+      title: 'Fifth Article',
+      author: 'Ilham Zaara',
+      body: `This is content for article one which is created by Naim Jeem`
+    }
+  ];
+
   res.render('index', {
     title: 'Knowledgebase',
-    head: 'Article'
+    articles: articles
   });
 });
 
